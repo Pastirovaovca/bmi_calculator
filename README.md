8594309ec1a3c4e4bf0a40cab5f015865206679f
Marko <pastirova.ovca@gmail.com>
2021-08-29 23:58:08 +0200

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+BMI calculator
+
+A simple calculation of body to mass index.
+App made for educational purpose.
---
+++ lib/components/round_icon_button.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class RoundIconButton extends StatelessWidget {
+  RoundIconButton({@required this.icon, @required this.onPress});
+
+  final IconData icon;
+  final Function onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      onPressed: onPress,
+      constraints: BoxConstraints.tightFor(width: 56, height: 56),
+      elevation: 0,
+      shape: CircleBorder(),
+      fillColor: Color(0xFF4C4F5E),
+      child: Icon(icon),
+    );
+  }
+}
